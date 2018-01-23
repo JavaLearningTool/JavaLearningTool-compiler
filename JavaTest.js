fdf68ac575b15f79a92d41d15142826c61e1f846
Carl Schriever <crschriever@gmail.com>
2018-01-22 22:07:24 -0500

    Queued testers

--- JavaTest.js
+++ JavaTest.js
@@ -15,46 +15,73 @@ function newLineToBreak(str) {
     return str.replace(/\n/g, "<br>");
 }
 
+function runProcess() {
+
+    processing = true;
+    let temp = queue.splice(0, 1)[0];
+
+    let code = temp.code;
+    let challenge = temp.challenge;
+    let finalCallback = temp.callback;
+
+    let callback = function(obj) {
+        processing = false;
+        finalCallback(obj);
+        if (queue.length > 0) {
+            runProcess();
+        }
+    };
+
+    let testCallback = (err, stdout, stderr) => {
+        if (err) {
+            console.log(`exec error: ${err}`);
+            console.log(`STDOUT: ${stdout}`);
+            console.log(`STDERR: ${stderr}`);
+            callback(JSON.stringify({error: stdout}));
+            return;
+        }
+
+        console.log(`bash STDOUT: ${stdout}`);
+        console.log(`bash STDOUT: ${stdout}`);
+        callback(escapeNewLine(stdout));
+    };
+
+    let makeFileCallback = () => {
+        exec('bash ChallengeTest.sh ' + challenge, testCallback);
+    };
+
+    // Create sandbox folder where everything is run
+    if (fs.existsSync('sandbox')) {
+        console.log("Sandbox folder already exists! Oops.");
+        callback({error: errorMessage});
+        return;
+    } else {
+        fs.mkdirSync('sandbox');
+    }
+
+    // Make the java folder to execute in
+    fs.writeFile('sandbox/Test.java', code, function(err) {
+        if (err) {
+            console.log("Error creating file");
+            callback({error: errorMessage});
+            return;
+        }
+        // This will run ChallengeTest.sh
+        makeFileCallback();
+    });
+}
+
+let queue = [];
+let processing = false;
+
 function JavaTester() {
     return {
-        runProcess: function(code, challenge, callback) {
-            let testCallback = (err, stdout, stderr) => {
-                if (err) {
-                    console.log(`exec error: ${err}`);
-                    console.log(`STDOUT: ${stdout}`);
-                    console.log(`STDERR: ${stderr}`);
-                    callback(JSON.stringify({error: stdout}));
-                    return;
-                }
-
-                console.log(`bash STDOUT: ${stdout}`);
-                console.log(`bash STDOUT: ${stdout}`);
-                callback(escapeNewLine(stdout));
-            };
-
-            let makeFileCallback = () => {
-                exec('bash ChallengeTest.sh ' + challenge, testCallback);
-            };
-
-            // Create sandbox folder where everything is run
-            if (fs.existsSync('sandbox')) {
-                console.log("Sandbox folder already exists! Oops.");
-                callback({error: errorMessage});
-                return;
-            } else {
-                fs.mkdirSync('sandbox');
+        addProcess(code, challenge, callback) {
+            queue.push({ code, challenge, callback });
+            console.log("Queue: ", queue.length);
+            if (!processing) {
+                runProcess();
             }
-
-            // Make the java folder to execute in
-            fs.writeFile('sandbox/Test.java', code, function(err) {
-                if (err) {
-                    console.log("Error creating file");
-                    callback({error: errorMessage});
-                    return;
-                }
-                // This will run ChallengeTest.sh
-                makeFileCallback();
-            });
         }
     };
 }
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ const express = require('express');
 
 const router = express.Router();
 const JavaTest = require('../JavaTest.js');
+const tester = JavaTest();
 
 router.post('/', function(req, res, next) {
-  let tester = JavaTest();
   console.log("SRC Code: " + req.body.src);
   console.log("Challenge: " + req.body.challenge);
   
-  tester.runProcess(req.body.src, req.body.challenge, function (answer) {
+  tester.addProcess(req.body.src, req.body.challenge, function (answer) {
     console.log(answer);
     res.send(answer);
-  })
+  });
 });
 
 module.exports = router;
