a5609e167367eacf032311d38fb646b9d3fbf011
crschriever <crschriever@gmail.com>
2018-03-26 20:56:05 -0400

    Better error messages

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const logger = require("./logger");
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const index = require('./routes/index');
+const index = require("./routes/index");
 
 const app = express();
 
@@ -13,26 +13,26 @@ app.use(morgan("dev", { stream: logger.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    let error = err.message;
+    error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
+    // json error page
+    res.status(err.status || 500);
+    res.json({ error });
 });
 
 module.exports = app;
